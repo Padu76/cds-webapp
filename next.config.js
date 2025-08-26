045c8a707b225159d0bfc0c158bdd0e46996a8a0
Padu76 <andrea.padoan@gmail.com>
2025-08-26 13:41:22 +0200

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -1,4 +1,38 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  experimental: {
+    serverComponentsExternalPackages: [
+      'googleapis',
+      'google-auth-library',
+      'mammoth',
+      'xlsx',
+      'pdf-parse'
+    ]
+  },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      // Escludi moduli Node.js dal bundle client-side
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+        net: false,
+        tls: false,
+        child_process: false,
+        readline: false,
+        os: false,
+        path: false,
+        crypto: false,
+        stream: false,
+        http: false,
+        https: false,
+        url: false,
+        zlib: false,
+        util: false
+      };
+    }
+    
+    return config;
+  }
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
